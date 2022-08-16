791818de334f8f00c50be8a72862f6b7d5ec426c
Cristian <cristianvasquez@gmail.com>
2022-08-16 18:53:24 +0200

    chore: refactor to simplify

--- examples/get-view-from-cube-demo.js
+++ examples/get-view-from-cube-demo.js
@@ -8,6 +8,12 @@ async function main () {
   const cube = await source.cube('https://ld.stadt-zuerich.ch/statistics/ZUS-BTA-ZSA')
   const view = View.fromCube(cube)
 
+  console.log('---------')
+  console.log('All cubes', view.cubes())
+
+  console.log('---------')
+  console.log('All sources', view.sources().map(x => x.endpoint))
+
   const observations = await view.observations()
   console.log('---------')
   console.log('view.observations().length', observations.length)
@@ -17,7 +23,10 @@ async function main () {
   console.log('count', count)
 
   console.log('---------')
-  console.log('All cubes', view.cubes())
+  for (const dimension of view.dimensions) {
+    const cubeDimension = dimension.cubeDimensions[0]
+    console.log('dimension', cubeDimension.path?.value, 'from cubes', dimension.cubes)
+  }
 }
 
 main()
